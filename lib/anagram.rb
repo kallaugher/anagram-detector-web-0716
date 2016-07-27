d44c689f3e58b52c6f80bae4428e47282505712e
kallaugher <alicekallaugher@gmail.com>
2016-07-27 18:38:19 -0400

    created Anagram class with #match method

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :words
+  attr_reader :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words)
+    words.delete_if {|word| word.split("").sort != @word.split("").sort}
+  end
+
+end
